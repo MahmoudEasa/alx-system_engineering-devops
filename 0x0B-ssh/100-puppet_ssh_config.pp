21fc87806b0d8b26b7ae406694f0013852d3bb5a
MahmoudEasa <mu01011422865@gmail.com>
2023-09-23 08:03:38 +0000

    Fix 100-puppet_ssh_config.pp 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # set up client SSH configuration file
 # to connect to a server without typing a password.
 
-file_line { '/etc/ssh/sshd_config':
-  line  => 'PasswordAuthentication no',
+file_line { 'configuration file':
+  path => '/etc/ssh/sshd_config',
+  line => 'PasswordAuthentication no',
 }
