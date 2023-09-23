7dc80c1c54e06f94c53dc497de7f45864651b514
MahmoudEasa <mu01011422865@gmail.com>
2023-09-23 08:24:50 +0000

    Fix 100-puppet_ssh_config.pp 5

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,7 @@
 # set up client SSH configuration file
 
-file_line { 'disable password login':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-file_line { 'add path to find the keys':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+file { '/etc/ssh/sshd_config':
+  ensure  => 'file',
+  content => 'IdentityFile ~/.ssh/school
+	      PasswordAuthentication no',
 }
