4c8cdc1438443339067c42f5c84b0a03ba5de791
MahmoudEasa <mu01011422865@gmail.com>
2023-09-23 08:06:44 +0000

    Fix 100-puppet_ssh_config.pp 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,3 +5,8 @@ file_line { 'configuration file':
   path => '/etc/ssh/sshd_config',
   line => 'PasswordAuthentication no',
 }
+
+file_line { 'add path':
+  path => '/etc/ssh/sshd_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
