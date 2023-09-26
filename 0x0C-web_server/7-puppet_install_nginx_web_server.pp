3e7f7fa0c99fa94eb53b2d55b2771809bb64b607
MahmoudEasa <mu01011422865@gmail.com>
2023-09-26 15:08:28 +0000

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# Install and configure an Nginx server using Puppet
+
+exec {'install_nginx':
+  command => 'sudo apt-get -y install nginx',
+}
+
+file {
+  ensure  => 'present',
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+}
+
+file_line {'redirect_me':
+  ensure   => 'present',
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => '\n\tlocation /redirect_me {\n\t\treturn 301 https://google.com;\n\t}\n',
+  multiply => true,
+}
+
+exec { 'restart_nginx':
+  command => 'sudo service nginx restart',
+}
