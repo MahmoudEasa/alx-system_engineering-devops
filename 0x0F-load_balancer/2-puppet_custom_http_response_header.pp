f7bcd86697f36148ebc6614f5aa1b37b2554e7ab
MahmoudEasa <mu01011422865@gmail.com>
2023-10-02 18:42:22 +0000

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Configure Nginx so that its HTTP response contains a custom header (on web-01 and web-02)
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+package { 'nginx':
+  ensure => 'installed',
+  name   => 'nginx',
+}
+
+file_line { 'nginx config':
+  path  => '/etc/nginx/nginx.conf',
+  line  => "\tadd_header X-Served-By ${hostname};",
+  after => 'http {',
+}
+
+exec { 'nginx restart':
+  command => '/usr/sbin/service nginx restart',
+}
