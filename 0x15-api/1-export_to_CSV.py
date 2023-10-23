16d0c8787209610512ee1704ca912f2e6dd4cb1a
MahmoudEasa <mu01011422865@gmail.com>
2023-10-23 15:19:20 +0000

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-"""
+""" Write a Python script that, using this REST API,
+    for a given employee ID,
+    returns information about his/her TODO list progress.
 """
 
 
 if __name__ == "__main__":
     import requests
+    from sys import argv
 
-    url = ""
-    req = requests.get(url)
-    print(req)
+    if (len(argv) == 2):
+        id = int(argv[1])
+        user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+        user = requests.get(user_url).json()
+        todo_url = f"https://jsonplaceholder.typicode.com/todos/"
+        req_todo = requests.get(todo_url).json()
+        todo_filter = list(filter(lambda obj: obj['userId'] == id, req_todo))
+
+        user_name = user['name']
+        file_name = f"{id}.csv"
+
+        with open(file_name, 'w') as file:
+            for task in todo_filter:
+                file.write(f'"{id}","{user_name}","{task["completed"]}",\
+"{task["title"]}"\n')
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-"""
+""" Write a Python script that, using this REST API,
+    for a given employee ID,
+    returns information about his/her TODO list progress.
 """
 
 
 if __name__ == "__main__":
     import requests
+    from sys import argv
 
-    url = ""
-    req = requests.get(url)
-    print(req)
+    if (len(argv) == 2):
+        id = int(argv[1])
+        user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+        user = requests.get(user_url).json()
+        todo_url = f"https://jsonplaceholder.typicode.com/todos/"
+        req_todo = requests.get(todo_url).json()
+        todo_filter = list(filter(lambda obj: obj['userId'] == id, req_todo))
+        done_filter = list(filter(lambda obj: obj['completed'], todo_filter))
+
+        user_name = user['name']
+        total_tasks = len(todo_filter)
+        done_tasks = len(done_filter)
+
+        print(f"Employee {user_name} is done with \
+tasks({done_tasks}/{total_tasks}):")
+        for task in done_filter:
+            print("\t ", end='')
+            print(task['title'])
