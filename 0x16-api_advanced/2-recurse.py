eb7d075550a8f461501744bfe9704fed0210f920
MahmoudEasa <MahmoudEasa@gmail.com>
2023-11-07 14:34:58 +0000

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+"""Recursive function that queries the Reddit API
+    and returns a list containing the titles of all
+    hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Recurse Function """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Test"}
+    params = {'after': after}
+    req = requests.get(url, headers=headers, params=params)
+
+    if req.status_code == 200:
+        data = req.json().get("data")
+        posts = data.get("children")
+        for post in posts:
+            title = post.get('data').get('title')
+            hot_list.append(title)
+        after = data.get('after')
+        if after is not None:
+            return (recurse(subreddit, hot_list, after))
+        return (hot_list)
+    else:
+        return (None)
