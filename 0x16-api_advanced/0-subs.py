1776daffdda6a2a707024cccc96d74ef13bb818e
MahmoudEasa <MahmoudEasa@gmail.com>
2023-11-07 13:00:53 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""Queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Test"}
+    req = requests.get(url, headers=headers)
+
+    if req.status_code == 200:
+        return (req.json().get("data").get("subscribers"))
+    else:
+        return (0)
