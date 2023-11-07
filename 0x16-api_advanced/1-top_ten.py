3aa7117edf647bdcb3b7c0ee2e1a726fa2545db6
MahmoudEasa <MahmoudEasa@gmail.com>
2023-11-07 14:13:36 +0000

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+"""Queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ Top_ten Function """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Test"}
+    params = {"limit": 10}
+    req = requests.get(url, headers=headers, params=params)
+
+    if req.status_code == 200:
+        posts = req.json().get("data").get("children")
+        for post in posts:
+            title = post.get('data').get('title')
+            print(title)
+    else:
+        print(None)
